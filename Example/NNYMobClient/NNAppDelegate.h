d2041a6ccca6e7e10b27282763c094a9bad9310c
numa08 <n511287@gmail.com>
2014-11-27 22:07:47 +0900

    Initial commit

---
+++ Example/NNYMobClient/NNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NNAppDelegate.h
+//  NNYMobClient
+//
+//  Created by CocoaPods on 11/27/2014.
+//  Copyright (c) 2014 numa08. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/NNYMobClient/NNViewController.h
@@ -0,0 +1,13 @@
+//
+//  NNViewController.h
+//  NNYMobClient
+//
+//  Created by numa08 on 11/27/2014.
+//  Copyright (c) 2014 numa08. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NNViewController : UIViewController
+
+@end
