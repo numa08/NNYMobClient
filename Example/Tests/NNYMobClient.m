6a83c6132d236c0bb81ee98022e0c64fdaf93816
numa08 <n511287@gmail.com>
2014-12-02 00:52:15 +0900

    Create instance for w303hw by name
    fixes #1

---
+++ Example/Tests/NNYMobClient.m
@@ -0,0 +1,21 @@
+//
+//  NNYMobClient.m
+//  NNYMobClient
+//
+//  Created by numa08 on 2014/12/02.
+//  Copyright (c) 2014年 numa08. All rights reserved.
+//
+
+//#import <NNYMobClient/NNYMobClient.h>
+
+SpecBegin(InitialSpecs)
+
+describe(@"Instantinate", ^{
+    
+    it(@"mathing name", ^{
+//        NNYMobClient *client = [NNYMobClient clientForName:NNYMobW303HW withIPAddress:@"192.168.128.1"];
+//        NSString *name = client.name; //W303HW
+//        expect(name).to.equal(@"W303HW");
+    });
+});
+SpecEnd
---
+++ w303hw/NNYmobW303HWClient.m
@@ -0,0 +1,30 @@
+//
+//  NNYmobW303HWClient.m
+//  Pods
+//
+//  Created by 船曳崇也 on 2014/12/04.
+//
+//
+
+#import "NNYmobW303HWClient.h"
+@interface NNYmobW303HWClient()
+@property (nonatomic, copy) NSString *ipaddress;
+@end
+
+@implementation NNYmobW303HWClient
+
+- (id)initWithIPAddress:(NSString *)ipaddress
+{
+    self = [super init];
+    if (self) {
+        self.ipaddress = ipaddress;
+    }
+    return self;
+}
+
+- (NSString*)name
+{
+    return NNYMobW303HW;
+}
+
+@end
